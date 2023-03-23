79f00cc9b6e91c68deebbdabba8ed26b95e2b153
mivanova <margarita_star92@mail.ru>
2023-03-23 14:43:21 +0600

    Added attribute types

---
+++ HardCode.TestTask.ProductCatalog.Repository/Migrations/20230323084119_AttributeTypes.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace HardCode.TestTask.ProductCatalog.Repository.Migrations
+{
+    /// <inheritdoc />
+    public partial class AttributeTypes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "attribute_type",
+                columns: new[] { "id", "name" },
+                values: new object[,]
+                {
+                    { 1, "decimal" },
+                    { 2, "integer" },
+                    { 3, "string" },
+                    { 4, "boolean" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "attribute_type",
+                keyColumn: "id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "attribute_type",
+                keyColumn: "id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "attribute_type",
+                keyColumn: "id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "attribute_type",
+                keyColumn: "id",
+                keyValue: 4);
+        }
+    }
+}
--- HardCode.TestTask.ProductCatalog.Repository/RepositoryContext.cs
+++ HardCode.TestTask.ProductCatalog.Repository/RepositoryContext.cs
@@ -1,5 +1,6 @@
 ﻿using HardCode.TestTask.ProductCatalog.Entities.Models;
 using Microsoft.EntityFrameworkCore;
+using Attribute = HardCode.TestTask.ProductCatalog.Entities.Models.Attribute;
 
 namespace HardCode.TestTask.ProductCatalog.Repository;
 
@@ -9,16 +10,33 @@ public class RepositoryContext : DbContext
         : base(options)
     {
     }
+    
+    public DbSet<Product>? Products { get; set; }
+    public DbSet<Category>? Categories { get; set; }
+    public DbSet<AttributeType>? AttributeTypes { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        SeedAttributeTypes(modelBuilder);
+        modelBuilder.Entity<Attribute>().Property(attribute => attribute.Id).ValueGeneratedOnAdd();
+        modelBuilder.Entity<AttributeType>().Property(type => type.Id).ValueGeneratedOnAdd();
+        modelBuilder.Entity<Category>().Property(category => category.Id).ValueGeneratedOnAdd();
+        modelBuilder.Entity<Product>().Property(product => product.Id).ValueGeneratedOnAdd();
         modelBuilder.Entity<ProductCategoryAttribute>()
             .HasKey(attribute => new { attribute.ProductId, attribute.AttributeId });
         
         base.OnModelCreating(modelBuilder);
     }
-
-    public DbSet<Product>? Products { get; set; }
-    public DbSet<Category>? Categories { get; set; }
-    public DbSet<AttributeType>? AttributeTypes { get; set; }
+    
+    private static void SeedAttributeTypes(ModelBuilder builder)
+    {
+        builder.Entity<AttributeType>(u =>
+        {
+            var @decimal = new AttributeType {Id = 1, Name = "decimal"};
+            var integer = new AttributeType {Id = 2, Name = "integer"};
+            var @string = new AttributeType {Id = 3, Name = "string"};
+            var boolean = new AttributeType {Id = 4, Name = "boolean"};
+            u.HasData(@decimal, integer, @string, boolean);
+        });
+    }
 }
